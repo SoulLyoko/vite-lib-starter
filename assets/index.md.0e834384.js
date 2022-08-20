ace8d5e305513ba28f35314babd343d4083d7230
SoulLyoko <48344443+SoulLyoko@users.noreply.github.com>
2022-08-20 07:44:29 +0000

    Deploying to gh-pages from @ SoulLyoko/vite-lib-starter@0ec827a1bc0bfd938b37783f23607eee7c976dd8 🚀

--- assets/index.md.0e834384.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,r as t,o,c as r,d as n,a as s}from"./app.04f2b4e9.js";const x='{"title":"","description":"","frontmatter":{"sidebar":false},"headers":[],"relativePath":"index.md"}',c={};function d(p,_,i,l,m,f){const e=t("markdown");return o(),r("div",null,[n("p",null,[s(e,{path:"/../README.md"})])])}var h=a(c,[["render",d]]);export{x as __pageData,h as default};
