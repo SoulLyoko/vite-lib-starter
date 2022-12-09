37984bad4eafba62a26e9b00dd65cd30918a175c
Lyoko <519052532@qq.com>
2022-12-09 17:07:23 +0800

    chore: fix demo

--- packages/components/input/demo/textarea.vue
+++ packages/components/input/demo/textarea.vue
@@ -1,6 +1,6 @@
 <template>
   <v-input v-model="value" type="textarea" placeholder="Input Textarea"></v-input>
-  <v-input v-model="value" type="textarea" rows="3" placeholder="Input Textarea"></v-input>
+  <v-input v-model="value" type="textarea" :rows="3" placeholder="Input Textarea"></v-input>
 </template>
 
 <script setup lang="ts">
